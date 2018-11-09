b7cb3f9e039704fee8dbfa8790922eaa766e5c7d
Samuel Almeida <samuel.ralmeida@gmail.com>
2018-11-09 08:34:15 -0200

    raindrops refactored

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -6,49 +6,20 @@ import (
 
 // Convert return Pling, Plang, Plong according factors
 func Convert(num int) string {
-	// store num to return if necessary
-	n := num
-
-	// store factors in map to search fastly
-	factors := map[int]bool{}
-
-	// first divide num until turn it in odd
-	for num%2 == 0 {
-		factors[2] = true
-		num = num / 2
-	}
-
-	// if num is odd, we can try divid it for odds too
-	// so we can skip two by two
-	for i := 3; i*i <= num; i = i + 2 {
-		for num%i == 0 {
-			factors[i] = true
-			num = num / i
-		}
-	}
-
-	// ifnum not divide for odds, so it is prime
-	if num > 2 {
-		factors[num] = true
-	}
-
-	resp := checkFactors(factors)
-	if resp == "" {
-		resp = strconv.Itoa(n)
-	}
-	return resp
-}
-
-func checkFactors(factors map[int]bool) string {
 	var resp string
-	if factors[3] {
+
+	if num%3 == 0 {
 		resp += "Pling"
 	}
-	if factors[5] {
+	if num%5 == 0 {
 		resp += "Plang"
 	}
-	if factors[7] {
+	if num%7 == 0 {
 		resp += "Plong"
+	}
+
+	if resp == "" {
+		resp = strconv.Itoa(num)
 	}
 	return resp
 }
